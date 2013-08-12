91e61de8d2b50f4b1658554e7a727d0c6c4fbf7e
Daniel Monteiro Basso <daniel@basso.inf.br>
2013-08-12 20:24:13 +0100

    starting main loop restructuring

--- entangled.py
+++ entangled.py
@@ -144,223 +144,245 @@ def draw_text(x,y,font,text,colour,surface=screen):
     x2,y2 = font.size(text)
     surface.blit(font.render(text,1,colour),(x-x2/2,y-y2/2))
 
-g_board = Board()   # Initialise game board
-status = 1
-points1 = 0
-
-new_tile = make_piece() # Make the next tile
-rep_tile = make_piece() # Make the reserved tile
-cont = 1
 
 xy_incs = [(1, 0), (1, 1), (0, 1), (-1, 0), (-1, -1), (0, -1)]
 xy_px_incs = [(75, 44), (0, 88), (-75, 44), (-75, -44), (0, -88), (75, -44)]
 xy_inc_map = dict((i + 1, xy_incs[i // 2]) for i in range(12))
 xy_px_inc_map = dict((i + 1, xy_px_incs[i // 2]) for i in range(12))
 
-while cont:
-    lc = rc = 0
-    for event in pg.event.get():
-        if event.type == pg.QUIT: cont = 0
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            if event.button == 1: lc = 1
-            if event.button == 3: rc = 1
 
-    if pg.key.get_pressed()[pg.K_ESCAPE]: break
-    mx,my = pg.mouse.get_pos()
+class Entangled:
+    def __init__(self):
+        self.done = False
+        self.new_game()
 
-    if rc and status == 1:  # Rotate tile
-        for i in new_tile[1]:
-            i[0] = (i[0]+2)%12
-            i[1] = (i[1]+2)%12
-            if i[0] == 0: i[0] = 12
-            if i[1] == 0: i[1] = 12
+    def new_game(self):
+        self.g_board = Board()   # Initialise game board
+        self.state = 1
+        self.score = 0
+        self.new_tile = make_piece() # Make the next tile
+        self.rep_tile = make_piece() # Make the reserved tile
 
-        piece = pg.Surface((101,100))  # Change the image of the tile
-        piece.set_colorkey(color.black)
-        path_img = pg.Surface((101,100))
-        path_img.set_colorkey(color.white)
-        path_img.set_alpha(254)
-        path_img.fill(color.white)
-        for b in range(10):
-            points = []
-            for i in range(6): points.append((50+cos(radians(i*60))*(50-b),50+sin(radians(i*60))*(50-b)))
-            pg.draw.polygon(piece,(255-b*10,202-b*10,131-b*10),points)
+    def check_events(self):
+        self.left_click = False
+        self.right_click = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                self.done = True
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                self.left_click = (event.button == 1)
+                self.right_click = (event.button == 3)
+        if pg.key.get_pressed()[pg.K_ESCAPE]: 
+            self.done = True
 
-        for i in new_tile[1]:   # Redraw the paths
-            x1 = 50+cos(radians(i[0]*30-15))*45
-            y1 = 50+sin(radians(i[0]*30-15))*45
-            x2 = 50+cos(radians(i[1]*30-15))*45
-            y2 = 50+sin(radians(i[1]*30-15))*45
-            if -1 <= i[0]-i[1] <= 1 and ((i[0] > i[1] and i[0]%2 < i[1]%2) \
-            or (i[0] < i[1] and i[0]%2 > i[1]%2)): width = 10
-            else: width = 5
-            pg.draw.line(path_img,color.black,(x1,y1),(x2,y2),width)
-        piece.blit(path_img,(0,0))
+    def main_loop(self):
+        while True:
+            self.check_events()
+            if self.done:
+                return
+            lc, rc = self.left_click, self.right_click
+            mx,my = pg.mouse.get_pos()
 
-        new_tile[0] = piece
+            if rc and self.state == 1:  # Rotate tile
+                for i in self.new_tile[1]:
+                    i[0] = (i[0]+2)%12
+                    i[1] = (i[1]+2)%12
+                    if i[0] == 0: i[0] = 12
+                    if i[1] == 0: i[1] = 12
 
-    elif lc and status == 1 and 0 < mx < 100 and 530 < my < 630:    # Swap current tile with reserve tile
-        new_tile,rep_tile = rep_tile,new_tile
+                piece = pg.Surface((101,100))  # Change the image of the tile
+                piece.set_colorkey(color.black)
+                path_img = pg.Surface((101,100))
+                path_img.set_colorkey(color.white)
+                path_img.set_alpha(254)
+                path_img.fill(color.white)
+                for b in range(10):
+                    points = []
+                    for i in range(6): points.append((50+cos(radians(i*60))*(50-b),50+sin(radians(i*60))*(50-b)))
+                    pg.draw.polygon(piece,(255-b*10,202-b*10,131-b*10),points)
 
-    elif lc and 640 < mx < 760 and 15 < my < 45:    # New game button
-        g_board = Board()
-        status = 1
-        points1 = 0
+                for i in self.new_tile[1]:   # Redraw the paths
+                    x1 = 50+cos(radians(i[0]*30-15))*45
+                    y1 = 50+sin(radians(i[0]*30-15))*45
+                    x2 = 50+cos(radians(i[1]*30-15))*45
+                    y2 = 50+sin(radians(i[1]*30-15))*45
+                    if -1 <= i[0]-i[1] <= 1 and ((i[0] > i[1] and i[0]%2 < i[1]%2) \
+                    or (i[0] < i[1] and i[0]%2 > i[1]%2)): width = 10
+                    else: width = 5
+                    pg.draw.line(path_img,color.black,(x1,y1),(x2,y2),width)
+                piece.blit(path_img,(0,0))
 
-        new_tile = make_piece()
-        rep_tile = make_piece()
-        paths_img.fill(color.black)
+                self.new_tile[0] = piece
 
-    elif lc and status == 1:    # Place down new tile and update path
-        # Change x,y coordinate
-        g_board.line1[0][0] += xy_inc_map[g_board.line1[1]][0]
-        g_board.line1[0][1] += xy_inc_map[g_board.line1[1]][1]
-        # Complex path changing
-        path_update = ((g_board.line1[1] - 1 & ~1) + 7) % 12
-        g_board.line1[1] = g_board.line1[1] % 2 + path_update
+            elif lc and self.state == 1 and 0 < mx < 100 and 530 < my < 630:    # Swap current tile with reserve tile
+                self.new_tile, self.rep_tile = self.rep_tile, self.new_tile
 
-        for i in range(len(g_board.coords)):
-            if g_board.coords[i][2] == g_board.line1[0]: break
-        else:
-            status = -1
-        g_board.pieces[i][0] = new_tile
+            elif lc and 640 < mx < 760 and 15 < my < 45:    # New game button
+                self.g_board = Board()
+                self.state = 1
+                self.score = 0
 
-        del new_tile
+                self.new_tile = make_piece()
+                self.rep_tile = make_piece()
+                paths_img.fill(color.black)
 
-    # Update the screen image
-    screen.fill(color.white)
-    trans_scr.fill(color.black)
-    g_board.draw_board(screen)
+            elif lc and self.state == 1:    # Place down new tile and update path
+                # Change x,y coordinate
+                self.g_board.line1[0][0] += xy_inc_map[self.g_board.line1[1]][0]
+                self.g_board.line1[0][1] += xy_inc_map[self.g_board.line1[1]][1]
+                # Complex path changing
+                path_update = ((self.g_board.line1[1] - 1 & ~1) + 7) % 12
+                self.g_board.line1[1] = self.g_board.line1[1] % 2 + path_update
 
-    for i in g_board.coords:
-        if i[2] == g_board.line1[0]:
-            x = i[0] + xy_px_inc_map[g_board.line1[1]][0]
-            y = i[1] + xy_px_inc_map[g_board.line1[1]][1]
-            in_map = 0
-            for i in g_board.coords:
-                if i[0:2] == (x,y): in_map = 1
-            if not in_map: status = -1
+                for i in range(len(self.g_board.coords)):
+                    if self.g_board.coords[i][2] == self.g_board.line1[0]: 
+                        break
+                else:
+                    self.state = -1
+                self.g_board.pieces[i][0] = self.new_tile
+
+                del self.new_tile
+
+            # Update the screen image
+            screen.fill(color.white)
+            trans_scr.fill(color.black)
+            self.g_board.draw_board(screen)
+
+            for i in self.g_board.coords:
+                if i[2] == self.g_board.line1[0]:
+                    x = i[0] + xy_px_inc_map[self.g_board.line1[1]][0]
+                    y = i[1] + xy_px_inc_map[self.g_board.line1[1]][1]
+                    in_map = 0
+                    for i in self.g_board.coords:
+                        if i[0:2] == (x,y): in_map = 1
+                    if not in_map: self.state = -1
 
 
 
-    if status == 1: # Not updating path condition
-        try: trans_scr.blit(new_tile[0],(x,y))  # Draw transparent tile
+            if self.state == 1: # Not updating path condition
+                try:
+                    trans_scr.blit(self.new_tile[0],(x,y))  # Draw transparent tile
+                except:
+                    for i in range(len(self.g_board.coords)):
+                        if self.g_board.coords[i][2] == self.g_board.line1[0]:
+                            break
 
-        except:
-            for i in range(len(g_board.coords)):
-                if g_board.coords[i][2] == g_board.line1[0]: break
+                    current_piece = self.g_board.pieces[i]
 
-            current_piece = g_board.pieces[i]
+                    if current_piece[0][1] != [] and self.state != -1:
+                        self.state = 0
 
-            if current_piece[0][1] != [] and status != -1:
-                status = 0
+                        if current_piece[0][1] == 1: self.state = -1
 
-                if current_piece[0][1] == 1: status = -1
+                        if self.state == 0:
+                            chain = 1
+                            self.score += chain
+                            for i in current_piece[0][1]:
 
-                if status == 0:
-                    chain = 1
-                    points1 += chain
-                    for i in current_piece[0][1]:
+                                draw_line = 0
+                                if self.g_board.line1[1] == i[0]:
+                                    self.g_board.line1[1] = i[1]
+                                    draw_line = 1
+                                elif self.g_board.line1[1] == i[1]:
+                                    self.g_board.line1[1] = i[0]
+                                    draw_line = 1
 
-                        draw_line = 0
-                        if g_board.line1[1] == i[0]:
-                            g_board.line1[1] = i[1]
-                            draw_line = 1
-                        elif g_board.line1[1] == i[1]:
-                            g_board.line1[1] = i[0]
-                            draw_line = 1
+                                if draw_line:
+                                    x1 = 50+cos(radians(i[0]*30-15))*45+current_piece[1][0]
+                                    y1 = 50+sin(radians(i[0]*30-15))*45+current_piece[1][1]
+                                    x2 = 50+cos(radians(i[1]*30-15))*45+current_piece[1][0]
+                                    y2 = 50+sin(radians(i[1]*30-15))*45+current_piece[1][1]
+                                    pg.draw.line(paths_img,color.red,(x1,y1),(x2,y2),3)
 
-                        if draw_line:
-                            x1 = 50+cos(radians(i[0]*30-15))*45+current_piece[1][0]
-                            y1 = 50+sin(radians(i[0]*30-15))*45+current_piece[1][1]
-                            x2 = 50+cos(radians(i[1]*30-15))*45+current_piece[1][0]
-                            y2 = 50+sin(radians(i[1]*30-15))*45+current_piece[1][1]
-                            pg.draw.line(paths_img,color.red,(x1,y1),(x2,y2),3)
+                            for i in self.g_board.coords:
+                                if i[2] == self.g_board.line1[0]:
+                                    x = i[0] + xy_px_inc_map[self.g_board.line1[1]][0]
+                                    y = i[1] + xy_px_inc_map[self.g_board.line1[1]][1]
 
-                    for i in g_board.coords:
-                        if i[2] == g_board.line1[0]:
-                            x = i[0] + xy_px_inc_map[g_board.line1[1]][0]
-                            y = i[1] + xy_px_inc_map[g_board.line1[1]][1]
+                            for i in range(len(self.g_board.coords)):
+                                if (x,y) == self.g_board.coords[i][0:2]: 
+                                    break
+                            next_piece = self.g_board.pieces[i]
 
-                    for i in range(len(g_board.coords)):
-                        if (x,y) == g_board.coords[i][0:2]: break
-                    next_piece = g_board.pieces[i]
+                            if next_piece[0][1] == []:
+                                self.state = 1
+                                self.new_tile = make_piece()
 
-                    if next_piece[0][1] == []:
-                        status = 1
-                        new_tile = make_piece()
+            elif self.state == 0:   # Still updating path condition
+                # Change x,y coordinate
+                self.g_board.line1[0][0] += xy_inc_map[self.g_board.line1[1]][0]
+                self.g_board.line1[0][1] += xy_inc_map[self.g_board.line1[1]][1]
+                # Change path end
+                path_update = ((self.g_board.line1[1] - 1 & ~1) + 7) % 12
+                self.g_board.line1[1] = self.g_board.line1[1] % 2 + path_update
 
-    elif status == 0:   # Still updating path condition
-        # Change x,y coordinate
-        g_board.line1[0][0] += xy_inc_map[g_board.line1[1]][0]
-        g_board.line1[0][1] += xy_inc_map[g_board.line1[1]][1]
-        # Change path end
-        path_update = ((g_board.line1[1] - 1 & ~1) + 7) % 12
-        g_board.line1[1] = g_board.line1[1] % 2 + path_update
+                for i in range(len(self.g_board.coords)):
+                    if self.g_board.coords[i][2] == self.g_board.line1[0]: break
 
-        for i in range(len(g_board.coords)):
-            if g_board.coords[i][2] == g_board.line1[0]: break
+                current_piece = self.g_board.pieces[i]
 
-        current_piece = g_board.pieces[i]
+                if current_piece[0][1] != [] and self.state != -1:
+                    self.state = 0
 
-        if current_piece[0][1] != [] and status != -1:
-            status = 0
+                    if current_piece[0][1] == 1: self.state = -1
 
-            if current_piece[0][1] == 1: status = -1
+                    if self.state == 0: # Award points for chaining
+                        chain += 1
+                        self.score += chain
+                        for i in current_piece[0][1]:
+                            draw_line = 0
+                            if self.g_board.line1[1] == i[0]:
+                                self.g_board.line1[1] = i[1]
+                                draw_line = 1
+                            elif self.g_board.line1[1] == i[1]:
+                                self.g_board.line1[1] = i[0]
+                                draw_line = 1
+                            if draw_line:
+                                x1 = 50+cos(radians(i[0]*30-15))*45+current_piece[1][0]
+                                y1 = 50+sin(radians(i[0]*30-15))*45+current_piece[1][1]
+                                x2 = 50+cos(radians(i[1]*30-15))*45+current_piece[1][0]
+                                y2 = 50+sin(radians(i[1]*30-15))*45+current_piece[1][1]
+                                pg.draw.line(paths_img,color.red,(x1,y1),(x2,y2),3)
 
-            if status == 0: # Award points for chaining
-                chain += 1
-                points1 += chain
-                for i in current_piece[0][1]:
-                    draw_line = 0
-                    if g_board.line1[1] == i[0]:
-                        g_board.line1[1] = i[1]
-                        draw_line = 1
-                    elif g_board.line1[1] == i[1]:
-                        g_board.line1[1] = i[0]
-                        draw_line = 1
-                    if draw_line:
-                        x1 = 50+cos(radians(i[0]*30-15))*45+current_piece[1][0]
-                        y1 = 50+sin(radians(i[0]*30-15))*45+current_piece[1][1]
-                        x2 = 50+cos(radians(i[1]*30-15))*45+current_piece[1][0]
-                        y2 = 50+sin(radians(i[1]*30-15))*45+current_piece[1][1]
-                        pg.draw.line(paths_img,color.red,(x1,y1),(x2,y2),3)
+                        for i in self.g_board.coords:
+                            if i[2] == self.g_board.line1[0]:
+                                x = i[0] + xy_px_inc_map[self.g_board.line1[1]][0]
+                                y = i[1] + xy_px_inc_map[self.g_board.line1[1]][1]
 
-                for i in g_board.coords:
-                    if i[2] == g_board.line1[0]:
-                        x = i[0] + xy_px_inc_map[g_board.line1[1]][0]
-                        y = i[1] + xy_px_inc_map[g_board.line1[1]][1]
+                        for i in range(len(self.g_board.coords)):
+                            if (x,y) == self.g_board.coords[i][0:2]: break
+                        next_piece = self.g_board.pieces[i]
 
-                for i in range(len(g_board.coords)):
-                    if (x,y) == g_board.coords[i][0:2]: break
-                next_piece = g_board.pieces[i]
+                        if next_piece[0][1] == []:  # Done chaining, make next tile
+                            self.state = 1
+                            self.new_tile = make_piece()
 
-                if next_piece[0][1] == []:  # Done chaining, make next tile
-                    status = 1
-                    new_tile = make_piece()
+            # Finish up drawing
+            screen.blit(trans_scr,(0,0))
+            screen.blit(paths_img,(0,0))
+            screen.blit(self.rep_tile[0],(0,530))
 
-    # Finish up drawing
-    screen.blit(trans_scr,(0,0))
-    screen.blit(paths_img,(0,0))
-    screen.blit(rep_tile[0],(0,530))
+            p_msg = fonts.p.render('Points: '+str(self.score),1,color.black)
+            screen.blit(p_msg,(10,10))
+            draw_text(700, 30, fonts.p, 'New Game',color.black)
+            if 640 < mx < 760 and 15 < my < 45:
+                pg.draw.rect(screen,color.black,(640,15,120,30),2)
 
-    p_msg = fonts.p.render('Points: '+str(points1),1,color.black)
-    screen.blit(p_msg,(10,10))
-    draw_text(700, 30, fonts.p, 'New Game',color.black)
-    if 640 < mx < 760 and 15 < my < 45:
-        pg.draw.rect(screen,color.black,(640,15,120,30),2)
+            self.g_board.draw_path(screen)
 
-    g_board.draw_path(screen)
+            if self.state == -1:    # Game ending condition
+                screen.blit(fonts.t1.render('Game Over',0,color.black),(150,250))
+                screen.blit(fonts.t1.render('Game Over',1,color.white),(150,250))
+                if self.score == 1:
+                    screen.blit(fonts.t2.render('You are a disgrace.',1,color.white),(100,310))
+                else:
+                    screen.blit(fonts.t2.render('You scored '+str(self.score)+' points.',1,color.white),(70,310))
 
-    if status == -1:    # Game ending condition
-        screen.blit(fonts.t1.render('Game Over',0,color.black),(150,250))
-        screen.blit(fonts.t1.render('Game Over',1,color.white),(150,250))
-        if points1 == 1:
-            screen.blit(fonts.t2.render('You are a disgrace.',1,color.white),(100,310))
-        else:
-            screen.blit(fonts.t2.render('You scored '+str(points1)+' points.',1,color.white),(70,310))
+            pg.display.flip()
+            pg.time.wait(30)
 
-    pg.display.flip()
-    pg.time.wait(30)
 
+if __name__ == '__main__':
+    game = Entangled()
+    game.main_loop()
 
